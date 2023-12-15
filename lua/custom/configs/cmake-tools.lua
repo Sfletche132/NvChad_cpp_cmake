8472fa2060735f418c8adb23afde25243e871686
saf <sam.fletcher@sonardyne.com>
2023-12-15 15:54:28 +0000

    Fixed cmake-tools config

--- lua/custom/configs/cmake-tools.lua
+++ lua/custom/configs/cmake-tools.lua
@@ -1,13 +1,15 @@
 local path = require "plenary.path"
 
 require("cmake-tools").setup {
-  cmake_build_directory = tostring(path:new("build", "${variant:buildType}")), -- this is used to specify generate directory for cmake
+  cmake_command = "cmake", -- this is used to specify cmake command path
+  ctest_command = "ctest", -- this is used to specify ctest command path
+  cmake_build_directory = "build", -- this is used to specify generate directory for cmake
   cmake_regenerate_on_save = true, -- Saves CMakeLists.txt file only if mofified.
   cmake_soft_link_compile_commands = true, -- if softlink compile commands json file
   -- cmake_compile_commands_from_lsp = true,
   cmake_build_options = { "-j32" },
   
-  cmake_kits_path = tostring(path:new("CMakeKits","CMakeKits.json")), --nearly there, need to reference it to location of this file
+  cmake_kits_path = nil, ---tostring(path:new("CMakeKits","CMakeKits.json")), --nearly there, need to reference it to location of this file
 
   cmake_executor = { -- executor to use
     name = "quickfix", -- name of the executor
